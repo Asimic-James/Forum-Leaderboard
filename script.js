abe312e3a6291a4f2d716f1e5622983fd2207ba7
asimic-james <jayasimic@gmail.com>
2024-03-07 23:02:28 +0100

    new project

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById('posts-container');
+
+const timeAgo = (time) => {};
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+        const data = await res.json();   
+        showLatestPosts(data);   
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+fetchData();
+
+const showLatestPosts = (data) => {
+   const {topic_list, users} = data;
+   const {topics} = topic_list;
+   postsContainer.innerHTML = topics.map((item) => {
+    const {
+        id, 
+        title, 
+        views, 
+        posts_count, 
+        slug, posters, 
+        category_id, 
+        bumped_at,
+    } = item;
+
+    return `
+    <tr>
+    <td>
+    <p class="post-title">${title}</p>
+    </td>
+    <td></td>
+    <td>${posts_count - 1}</td>
+    <td>${views}</td>
+    <td></td>
+    </tr>
+    `;
+
+   }).join('');
+};
